e2e89f838c979adc481a45493a18a6292185cc0a
Connor McDonald <30600369+clmcdonald@users.noreply.github.com>
2019-02-17 10:39:46 -0600

    Updated UI to work better with iOS

--- tabs/Settings.js
+++ tabs/Settings.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Picker, Text, View, StyleSheet, ScrollView, Button, TouchableOpacity, Alert, AsyncStorage, Linking } from 'react-native';
-import { Container, Header, Content, Accordion, Form, Item, Input, Label } from "native-base";
+import { Container, Header, Content, Accordion, Form, Icon, Item, Input, Label } from "native-base";
 import * as firebase from 'firebase';
 
 const TUTORIAL_COMPLETED_KEY = "tutorialCompleted";
@@ -28,10 +28,10 @@ export default class Settings extends Component {
 
   updateClassification = (newClassification) => {
     if(newClassification != null && (newClassification === "student" || newClassification === "alumni")) {
-      AsyncStorage.setItem('userClassification', newClassification);
       this.setState({
         userClassification: newClassification
       });
+      AsyncStorage.setItem('userClassification', newClassification);
 
       let userRef = firebase.database().ref("Users/" + this.state.userID + "/");
       userRef.update({
@@ -56,10 +56,22 @@ export default class Settings extends Component {
     this.props.navigation.navigate('Login');
   }
 
+  showAlert = () => {
+    Alert.alert(
+      'Change Classification',
+      'Please select whether you are a student or an alumnus.',
+      [
+        {text: 'Student', onPress: () => this.updateClassification("student") },
+        {text: 'Alumnus', onPress: () => this.updateClassification("alumni") },
+      ],
+      { cancelable: false }
+    )
+  }
+
   clearCookies() {
     RCTNetworking.clearCookies((cleared) => {
       console.log('Cookies cleared, had cookies=' + cleared.toString());
-    })
+    });
   }
 
   async componentDidMount() {
@@ -74,18 +86,24 @@ export default class Settings extends Component {
   }
 
   render() {
+    //Get the classification with the first letter capitalized
+    let capitalized = this.state.userClassification;
+    capitalized = capitalized.substring(0, 1).toUpperCase() + capitalized.substring(1);
+
     return (
       <ScrollView>
         <View style={styles.container}>
           <View style={styles.row}>
-            <Text style={styles.settingsLabel}>Classification: </Text>
-            <Picker style={{height: 50, width: '40%'}} selectedValue={this.state.userClassification} onValueChange={this.updateClassification}>
-              <Picker.Item label="Student" value="student" />
-              <Picker.Item label="Alumni" value="alumni" />
-            </Picker>
+            <Text style={styles.settingsText}>Classification: </Text>
+            <TouchableOpacity onPress={this.showAlert}>
+              <View style={styles.editButton}>
+                <Text style={styles.settingsText}>{capitalized + " "}</Text>
+                <Icon type="FontAwesome" name='pencil' style={styles.settingsText} />
+              </View>
+            </TouchableOpacity>
           </View>
 
-          <View style={styles.row}>
+          <View style={[styles.row, styles.logoutRow]}>
             <Button onPress={this.logout} title="Log Out" color="#841584" />
           </View>
         </View>
@@ -97,6 +115,8 @@ export default class Settings extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 15,
+    paddingBottom: 15,
   },
   row: {
     flex: 1,
@@ -106,7 +126,18 @@ const styles = StyleSheet.create({
     marginTop: 5,
     marginBottom: 5,
   },
-  settingsLabel: {
+  logoutRow: {
+    paddingTop: 20
+  },
+  editButton: {
+    backgroundColor: 'lightgray',
+    borderRadius: 5,
+    padding: 3,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  settingsText: {
     fontSize: 16,
   }
 });
