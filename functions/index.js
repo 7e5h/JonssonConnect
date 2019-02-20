2b8438602d0160d58a2b1e9528fc232a7b03a52b
Daniel Reiling <djr150130@utdallas.edu>
2019-02-20 15:46:22 -0600

    Added firebase cloud functions code to project

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,43 @@
 const functions = require('firebase-functions');
+var fetch = require('node-fetch')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase);
+
+exports.sendPushNotification = functions.database.ref('Events/{id}').onCreate(event => {
+
+    const root = event.data.ref.root
+    var messages = []
+
+    //return the main promise
+    return root.child('/Users').once('value').then(function (snapshot) {
+
+        snapshot.forEach(function (childSnapshot) {
+
+            var expoToken = childSnapshot.val().notificationToken;
+
+            if (expoToken) {
+
+                messages.push({
+                    "to": expoToken,
+                    "body": "New Event Added"
+                })
+            }
+        })
+
+        return Promise.all(messages)
+
+    }).then(messages => {
+
+        fetch('https://exp.host/--/api/v2/push/send', {
+
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(messages)
+        })
+        return true;
+    })
+})
