445b9c22afe9d5b2e5acdd19ece9dd6b6d2f84dd
Connor McDonald <30600369+clmcdonald@users.noreply.github.com>
2019-03-02 15:06:40 -0600

    Pull news articles from UTDallas News Center.
    Also, several other improvements:
    Loading symbol on home page is now centered, used a Touchable component for each article on the home page, display News Center articles in a WebView component.

--- tabs/ArticleDetails.js
+++ tabs/ArticleDetails.js
@@ -5,122 +5,113 @@
  */
 
  import React, { Component } from 'react';
- import { ActivityIndicator, Image, ListView, StyleSheet, View } from 'react-native';
- import { Container,Content,CardItem, Icon, Text, Body } from 'native-base';
+ import { ActivityIndicator, Image, ListView, StyleSheet, View, WebView } from 'react-native';
+ import { Container, Content, CardItem, Icon, Text, Body } from 'native-base';
 
  export default class ArticleDetails extends Component {
 
-     constructor(props) {
-         super(props);
+  constructor(props) {
+    super(props);
 
-         this.state = {
-             isLoading: true
-         }
-     }
+    this.state = {
+      webviewLoaded: false
+    }
+  }
 
-     componentDidMount() {
-         this.fetchArticle()
-     }
+  renderLoading = () => {
+    <ActivityIndicator style={{paddingTop: 10}} />
+  }
 
-     fetchArticle = () => {
-         return fetch('https://jonssonconnect.firebaseio.com/Articles.json')
-             .then((response) => response.json())
-             .then((responseJson) => {
+  render() {
+    let article = this.props.navigation.state.params.item[1];
 
-                 let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    if(article.articleType === "News Center") {
+      //Article was retrieved from UTD News Center and should be loaded in a browser
 
-                 this.setState({
-                     isLoading: false,
-                     dataSource: ds.cloneWithRows(responseJson),
-                 });
+      return (
+        <View style={{flex: 1}}>
+          <WebView
+            source={{uri: article.newsCenterURL}}
+            onLoadEnd={() => {
+              this.setState({ webviewLoaded: true });
+            }}
+            startInLoadingState={true}
+          />
+        </View>
+      );
+    } else {
+      //Article was retrieved from Firebase and should be displayed
 
-             })
-             .catch((error) => {
-                 console.error(error);
-             });
-     }
+      return (
+        <Container>
+         <Content>
+           <Image source={{uri: article.articleImageURL}}  style={{ height: 300 }}></Image>
 
-     render() {
-
-         if (this.state.isLoading) {
-             return (
-                 <View style={{ flex: 1, paddingTop: 20 }}>
-                     <ActivityIndicator />
-                 </View>
-             );
-         }
-
-         return (
-             <Container>
-                 <Content>
-                     <Image source={ {uri: this.props.navigation.state.params.item[1].articleImageURL} }  style={{ height: 300 }}></Image>
-
-                     <CardItem>
-                         <Body>
-                         <Text style={styles.articleNameStyle}>{this.props.navigation.state.params.item[1].articleName}</Text>
-                         <Text style={{fontSize: 14, fontWeight: '800'}}></Text>
-                         <Text style={{fontSize: 14, color: '#808080',}}><Icon type='SimpleLineIcons'  name='clock' style={{ fontSize: 14}}/> Published on {
-                             new Date(this.props.navigation.state.params.item[1].postedOn).toLocaleDateString("en-US", { month:'short', day:'numeric', year:'numeric' })
-                         }</Text>
-                         {/* <Text style={{fontSize: 12, fontWeight: '100', color: "#b6b6b6",}}>_____________________________________________________________</Text> */}
-                         <Text style={styles.contentStyle}>{'\n\n\t\t\t\t\t' + this.props.navigation.state.params.item[1].articleContent + '\n\n'}</Text>
-                         <Text style={{fontSize: 10, fontWeight: '100', color:'#b6b6b6', alignSelf: 'center'}}> --- End of News Article ---</Text>
-                         </Body>
-                     </CardItem>
-
-                 </Content>
-             </Container>
-         )
-     }
- }
+           <CardItem>
+             <Body>
+               <Text style={styles.articleNameStyle}>{article.articleName}</Text>
+               <Text style={{fontSize: 14, fontWeight: '800'}}></Text>
+               <Text style={{fontSize: 14, color: '#808080',}}><Icon type='SimpleLineIcons'  name='clock' style={{ fontSize: 14}}/> Published on {
+                   new Date(article.postedOn).toLocaleDateString("en-US", { month:'short', day:'numeric', year:'numeric' })
+               }</Text>
+               <Text style={styles.contentStyle}>{'\n\n' + article.articleContent + '\n\n'}</Text>
+               <Text style={{fontSize: 10, fontWeight: '100', color:'#b6b6b6', alignSelf: 'center'}}> --- End of News Article ---</Text>
+             </Body>
+           </CardItem>
+         </Content>
+        </Container>
+      );
+    }
+  }
+}
 
 const styles = StyleSheet.create({
-    articleNameStyle: {
-        fontWeight: '800',
-        fontSize: 24,
-    },
-    contentStyle: {
-        fontWeight: '300',
-        fontSize: 15,
-    },
-    nameStyle: {
-        fontWeight: '600',
-        fontSize: 16,
-    },
-    descriptionStyle: {
-        fontWeight: '400',
-        fontSize: 12,
-    },
-    hostStyle: {
-        fontSize: 12,
-        color: '#808080',
-    },
-    eventNameStyle: {
-        fontSize: 12,
-    },
-    eventDescriptionStyle: {
-        fontSize: 10,
-    },
-    buttonStyle: {
-        fontSize: 12,
-    },
-    search: {
-        backgroundColor: '#FFFFFF',
-        borderWidth: 1,
-        borderRadius: 2,
-        borderColor: '#ddd',
-        borderBottomWidth: 0,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        elevation: 1,
-    },
-    searchbarColor: {
-        backgroundColor: '#0039A6',
-    },
-    searchButton: {
-        fontSize: 12,
-        color: '#ffffff',
-    },
+  articleNameStyle: {
+    fontWeight: '800',
+    fontSize: 24,
+  },
+  contentStyle: {
+    fontWeight: '300',
+    fontSize: 15,
+  },
+  nameStyle: {
+    fontWeight: '600',
+    fontSize: 16,
+  },
+  descriptionStyle: {
+    fontWeight: '400',
+    fontSize: 12,
+  },
+  hostStyle: {
+    fontSize: 12,
+    color: '#808080',
+  },
+  eventNameStyle: {
+    fontSize: 12,
+  },
+  eventDescriptionStyle: {
+    fontSize: 10,
+  },
+  buttonStyle: {
+    fontSize: 12,
+  },
+  search: {
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+  },
+  searchbarColor: {
+    backgroundColor: '#0039A6',
+  },
+  searchButton: {
+    fontSize: 12,
+    color: '#ffffff',
+  },
 });
